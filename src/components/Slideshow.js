62068b82af209bbecd9a81200aa1886be9fa56fb
alejandra <axjimenez89@gmail.com>
2024-02-20 16:33:18 -0500

    rebase

---
+++ src/components/Slideshow.js
@@ -0,0 +1,14 @@
+const Slideshow = () => {
+return (
+  <div className="container">
+    <img />
+    <div className="wrapper">
+      <button> {'>'} </button>
+      <p>title goes here</p>
+      <button> {'<'} </button>
+    </div>
+  </div>
+)
+}
+
+export default Slideshow;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Slideshow from "./components/Slideshow";
+// import { getDogs } from "./dogapi";
+// import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// TODO replace with your slideshow App
+const App = () => {
+  
+  return (
+    <section>
+      <h1>HELLO</h1>
+      <Slideshow />
+    </section>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
