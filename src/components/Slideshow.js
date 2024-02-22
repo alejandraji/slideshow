0dfa2bdfab0b431b7a7157b1b227a7587c32c0b6
alejandra <axjimenez89@gmail.com>
2024-02-21 22:22:04 -0500

    adding functionaliy

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,11 +1,34 @@
+import { useState, useEffect } from "react";
+import { getDogs } from "../dogapi";
+
 const Slideshow = () => {
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    const getDogData = getDogs();
+    getDogData.then((data) => {
+      setDogs(data);
+    });
+    return () => {};
+  })
+
+  //click events go here
+  const handleNext = () =>{
+    console.log('next')
+  }
+  const handlePrev = () => {
+    console.log('prev')
+  }
+
+
+
 return (
   <div className="container">
-    <img />
+    <img src={dogs[0].url}/>
     <div className="wrapper">
-      <button> {'>'} </button>
-      <p>title goes here</p>
-      <button> {'<'} </button>
+      <button onClick={handleNext}> {'>'} </button>
+      <p></p>
+      <button onClick={handlePrev}> {'<'} </button>
     </div>
   </div>
 )
